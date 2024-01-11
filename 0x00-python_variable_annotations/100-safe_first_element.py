ea0b10c862dc92fc0fed15c6a187e3f9d4bcc9f5
dominic-source <chinonsodomnic@gmail.com>
2024-01-11 23:02:43 +0100

    sequence

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 """This module using python type annotation to add values"""
+
 from types import NoneType
 from typing import Any, Sequence, Union
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
+def safe_first_element(lst: Sequence[Any]) -> Union[NoneType, Sequence]:
     """Dock type annotation implementation"""
     if lst:
         return lst[0]
