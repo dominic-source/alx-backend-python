b66cedd4d739648c5124030cf10f8c8ae65c3d66
dominic-source <chinonsodomnic@gmail.com>
2024-01-11 20:31:24 +0100

    concat string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""This module using python type annotation to add values"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatenate two strings"""
+
+    return str1+str2
