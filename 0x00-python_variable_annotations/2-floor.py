666d1b22d6c0a9ce4d91bdf9d5d32a2a5fd13fbf
dominic-source <chinonsodomnic@gmail.com>
2024-01-11 20:48:04 +0100

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""This module using python type annotation to add values"""
+import math
+
+
+def floor(n: float) -> int:
+    """Return a floor of the float"""
+
+    return math.floor(n)
