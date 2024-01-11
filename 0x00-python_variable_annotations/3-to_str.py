3a7fc17fc24b7fe62b677f0016f15325f9fab5a5
dominic-source <chinonsodomnic@gmail.com>
2024-01-11 20:57:57 +0100

    float to str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""This module using python type annotation to add values"""
+
+
+def to_str(n: float) -> str:
+    """A string representation of the float"""
+    return str(n)
