c75a13689bb3f553fd639613de3255e942809179
dominic-source <chinonsodomnic@gmail.com>
2024-01-11 22:19:22 +0100

    annotate a function

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -5,4 +5,5 @@ from typing import Tuple, Iterable, Sequence, List
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Annotate the function using Iterable, Sequence, List, and Turple"""
     return [(i, len(i)) for i in lst]
