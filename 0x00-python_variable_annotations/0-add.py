694d16dd40c7235aebd9138ff48ab297e1e3a45d
dominic-source <chinonsodomnic@gmail.com>
2024-01-11 20:15:55 +0100

    add annotation

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,8 +2,9 @@
 
 """This module using python type annotation to add values"""
 
+
 def add(a: float, b: float) -> float:
-    """This function will add two floating point number 
+    """This function will add two floating point number
     and return a float"""
 
     return a + b
