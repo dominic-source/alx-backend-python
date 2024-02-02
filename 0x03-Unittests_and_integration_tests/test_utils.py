ddbaa33fccd8527632d52262fc0d9bee0de18bfc
dominic-source <chinonsodomnic@gmail.com>
2024-02-02 23:54:16 +0100

    Solved: test the Mocking capability of the method using unittest and parameterized

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,8 +4,10 @@
 """
 
 import unittest
+from unittest.mock import patch, Mock, MagicMock
 from parameterized import parameterized, parameterized_class
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+import json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -27,3 +29,24 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test if an exception would be raised"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map=nested_map, path=path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Mock a test to get json"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+        ])
+    @patch("utils.requests")
+    def test_get_json(self, test_url, test_payload, mock_req):
+        """The test for getting json"""
+
+        mock_res = MagicMock()
+        mock_res.json.return_value = test_payload
+
+        mock_req.get.return_value = mock_res
+
+        self.assertEqual(get_json(test_url), test_payload)
+
+        mock_req.get.assert_called_once()
