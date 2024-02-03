b75a5a7f909a00b8a15028b27659d9dea9674add
dominic-source <chinonsodomnic@gmail.com>
2024-02-03 22:54:26 +0100

    solved: test public repos url

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 """
-This module is used to test client 
+This module is used to test client
 """
 
 import unittest
-from unittest.mock import patch, Mock, MagicMock
+from unittest.mock import patch, Mock, MagicMock, PropertyMock
 from parameterized import parameterized, parameterized_class
 from client import GithubOrgClient
 
@@ -21,3 +21,13 @@ class TestGithubOrgClient(unittest.TestCase):
         production = GithubOrgClient(org)
         production.org
         mock_method.assert_called_once()
+
+    def test_public_repos_url(self):
+        """Test public repos url"""
+
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock_method:
+            mock_method.return_value = {"repos_url": True}
+            production = GithubOrgClient("google")
+            self.assertEqual(production._public_repos_url,
+                             mock_method()["repos_url"])
