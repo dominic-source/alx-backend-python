c1e88154875cd30920c23c8abd05ebc1ac033dc3
dominic-source <chinonsodomnic@gmail.com>
2024-02-03 23:45:47 +0100

    solved: write test for public repos

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -8,6 +8,7 @@ import unittest
 from unittest.mock import patch, Mock, MagicMock, PropertyMock
 from parameterized import parameterized, parameterized_class
 from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -31,3 +32,19 @@ class TestGithubOrgClient(unittest.TestCase):
             production = GithubOrgClient("google")
             self.assertEqual(production._public_repos_url,
                              mock_method()["repos_url"])
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_method):
+        """Test the public repos"""
+        payload = TEST_PAYLOAD[0][1]
+        mock_method.return_value = payload
+
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_met:
+            mock_met.return_value = TEST_PAYLOAD[0][0]
+
+            production = GithubOrgClient("google")
+            production.public_repos()
+
+        mock_met.assert_called_once()
+        mock_method.assert_called_once()
